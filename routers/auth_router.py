5a5e138bd3a520a1e4ec54c379f900f129aea7a9
yandraofficial01-cpu <yandraofficial01@gmail.com>
2026-07-29 01:28:08 +0700

    Update auth_router.py

--- routers/auth_router.py
+++ routers/auth_router.py
@@ -1,40 +1,22 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-from pydantic import BaseModel, EmailStr
-
 from database import get_db
-from models import UserShowroom
-from auth import hash_password, verify_password # <-- HAPUS TITIK 2 NYA
+from models import UserShowroom, Showroom
+from auth import verify_password # cuma butuh verify
+import schemas
 
-router = APIRouter(prefix="/auth", tags=["Auth"])
-
-class UserRegister(BaseModel):
-    email: EmailStr
-    password: str
-    showroom_id: int 
-
-class UserLogin(BaseModel):
-    email: EmailStr
-    password: str
-
-@router.post("/register")
-def register(user: UserRegister, db: Session = Depends(get_db)):
-    cek = db.query(UserShowroom).filter(UserShowroom.email == user.email).first()
-    if cek: 
-        raise HTTPException(status_code=400, detail="Email sudah terdaftar")
-    
-    hashed = hash_password(user.password)
-    new_user = UserShowroom(email=user.email, password=hashed, showroom_id=user.showroom_id)
-    db.add(new_user); db.commit(); db.refresh(new_user)
-    return {"msg": "Register berhasil", "id": new_user.id, "showroom_id": new_user.showroom_id}
+router = APIRouter(prefix="/auth", tags=["Auth Showroom"])
 
 @router.post("/login")
-def login(user: UserLogin, db: Session = Depends(get_db)):
-    db_user = db.query(UserShowroom).filter(UserShowroom.email == user.email).first()
-    if not db_user: 
-        raise HTTPException(status_code=400, detail="Email atau password salah")
+def login_showroom(data: schemas.LoginRequest, db: Session = Depends(get_db)):
+    db_user = db.query(UserShowroom).filter(UserShowroom.email == data.email).first()
+    if not db_user or not verify_password(data.password, db_user.password):
+        raise HTTPException(status_code=401, detail="Email atau password salah")
     
-    if not verify_password(user.password, db_user.password):
-        raise HTTPException(status_code=400, detail="Email atau password salah")
-    
-    return {"msg": "Login berhasil", "showroom_id": db_user.showroom_id, "email": db_user.email}
+    showroom = db.query(Showroom).filter(Showroom.id == db_user.showroom_id).first()
+    return {
+        "message": "Login Berhasil", 
+        "showroom_id": showroom.id,
+        "subdomain": showroom.subdomain,
+        "nama_showroom": showroom.nama_showroom
+    }
