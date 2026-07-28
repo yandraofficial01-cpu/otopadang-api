8f65b1e02a605e04af307f178aab98ab7b859252
yandraofficial01-cpu <yandraofficial01@gmail.com>
2026-07-28 22:58:22 +0700

    Update database.py

--- database.py
+++ database.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base # <-- GANTI INI, yg lama udah deprecated
 from sqlalchemy.orm import sessionmaker
 
 # HARDCODE BUAT RAILWAY - JANGAN PAKE ENV DULU
