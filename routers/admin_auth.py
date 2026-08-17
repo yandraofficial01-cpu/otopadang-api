ee52f1557e5f0b0e1691d407b00598a1f090d370
yandraofficial01-cpu <yandraofficial01@gmail.com>
2026-08-17 20:01:26 +0700

    Update admin_auth.py

--- routers/admin_auth.py
+++ routers/admin_auth.py
@@ -1,15 +1,8 @@
 from fastapi import Depends, HTTPException, status
-from .auth_router import get_current_user
-from models.user import User
+from.auth_router import get_current_user
+from models import User
 
 def require_admin(current_user: User = Depends(get_current_user)):
-    """
-    Penjaga khusus buat pintu admin.
-    Harus login dulu, terus role nya harus 'admin'
-    """
-    if current_user.role != "admin":
-        raise HTTPException(
-            status_code=status.HTTP_403_FORBIDDEN,
-            detail="Akses ditolak. Khusus Admin."
-        )
+    if current_user.role!= "admin":
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Akses ditolak. Khusus Admin.")
     return current_user
